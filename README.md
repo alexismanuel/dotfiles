28925854704a707b4f741d4ebae20c2405ece1ca
alexismanuel <alexis.manuelpro@gmail.com>
2024-07-08 22:51:08 +0200

    add install script

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+my dotfiles
+
+## Install
+```bash
+curl https://raw.githubusercontent.com/alexismanuel/dotfiles/master/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+git clone --bare git@github.com:alexismanuel/dotfiles.git $HOME/.dotfiles
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles from git@github.com:alexismanuel/dotfiles.git";
+  else
+    echo "Moving existing dotfiles to ~/.dotfiles-backup";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
