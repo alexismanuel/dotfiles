377d8ed9b1a0b6e0ba77ee7af8a98e2e6364f432
Alexis <alexis.manuelpro@gmail.com>
2024-07-08 23:16:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles from git@github.com:alexismanuel/dotfiles.git";
   else
     echo "Moving existing dotfiles to ~/.dotfiles-backup";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
 fi
 
 dotfiles checkout
