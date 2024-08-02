5e22bbf77139034eb24f4f2459dff585f5479a22
alexismanuel <alexis.manuelpro@gmail.com>
2024-08-02 21:23:17 +0200

    set floatty term

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -233,11 +233,26 @@ require('mason-lspconfig').setup({
 
 vim.keymap.set('n', '<leader>t', ':ToggleTerm<CR>')
 require('toggleterm').setup{
-	shade_terminal = false,
-	direction = 'horizontal',
-	border = 'curved',
-	persist_mode = true,
-	shell = bash,
+	size = 20,
+	open_mapping = [[<c-\>]],
+	hide_numbers = true,
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 2,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true,
+	shell = vim.o.shell,
+	float_opts = {
+		border = "curved",
+		winblend = 0,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
 }
 
 function _G.set_terminal_keymaps()
