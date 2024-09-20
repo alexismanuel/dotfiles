93faff94d76bd9d7f297ac2a879112987e1faeda
alexismanuel <alexis.manuelpro@gmail.com>
2024-09-20 09:41:52 +0200

    telescope search for hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,19 @@
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, {})
+local telescope = require('telescope')
+local custom = {}
+telescope.setup {
+	defaults = {
+		prompt_position = 'top',
+		layout_strategy = 'horizontal',
+		sorting_strategy = 'ascending',
+	}
+}
+custom.find_files = function()
+	telescope_builtin.find_files {
+		find_command = { 'rg', '--files', '--iglob', '!.git', '--iglob', '!.venv', '--hidden' },
+		previewer = false
+	}
+end
+vim.keymap.set('n', '<leader>ff', custom.find_files, {})
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
